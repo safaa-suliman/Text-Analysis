389b2002cfe070eb87116e9df3a611df1964c6e8
safaa-suliman <120365359+safaa-suliman@users.noreply.github.com>
2024-12-09 15:28:03 +0200

    Update RightVersion.py

--- pythonproject/RightVersion.py
+++ pythonproject/RightVersion.py
@@ -123,6 +123,19 @@ def nmf_topic_modeling_on_specific_word(texts, num_topics=3):
 # Streamlit App
 st.title("📂 Document Analysis - Enhanced Features")
 
+
+# Log nltk data paths
+nltk.data.path.append('./nltk_data')
+st.warning(f"NLTK Data Paths:. is pdf with image data", nltk.data.path)
+
+# Check if required resources exist
+punkt_path = os.path.join('./nltk_data', 'tokenizers', 'punkt')
+st.warning(f"NLTK Data Paths:. is pdf with image data", os.path.join('./nltk_data', 'corpora', 'stopwords.zip')
+
+print("Punkt exists:", os.path.exists(punkt_path))
+print("Stopwords exists:", os.path.exists(stopwords_path))
+
+
 # File uploader for multiple PDFs
 uploaded_files = st.file_uploader("Upload multiple PDF files", type="pdf", accept_multiple_files=True)
 
